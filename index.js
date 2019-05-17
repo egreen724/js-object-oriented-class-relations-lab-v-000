f11871f5564d7e65693e1e816385a9deb38be1fc
Erin Green <erin.y.green@gmail.com>
2019-05-17 19:09:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,9 @@ class Driver {
   }
 
   passengers() {
-    return store.passengers.filter(
-      function(passenger) {
-        return passenger.trips
-      }.bind(this)
-    );
+    return this.trips().map(trip => {
+      return trip.passenger();
+    })
   }
 }
 
